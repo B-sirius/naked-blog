c2d23be2843f41b2ff17c7e9097ddfcbb9ff1f87
b-sirius <padfoot_07@163.com>
2018-11-22 11:28:57 +0800

    production fix

---
+++ build/precache-manifest.799903ddd4e6aef375b058345459eb1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3f78adbc54ada2092479",
+    "url": "/static/js/main.3f78adbc.chunk.js"
+  },
+  {
+    "revision": "a11c9289eccf932cdbf3",
+    "url": "/static/js/1.a11c9289.chunk.js"
+  },
+  {
+    "revision": "3f78adbc54ada2092479",
+    "url": "/static/css/main.2a45351e.chunk.css"
+  },
+  {
+    "revision": "35d61bd411cd2c8460602668f55fce5f",
+    "url": "/index.html"
+  }
+];
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -215,6 +215,11 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      'Components': path.resolve(__dirname, '../src/components'),
+      'Posts': path.resolve(__dirname, '../src/posts'),
+      'Util': path.resolve(__dirname, '../src/util'),
+      'Constant': path.resolve(__dirname, '../src/constant'),
+      'Assets': path.resolve(__dirname, '../src/assets')
     },
     plugins: [
       // Adds support for installing with Plug'n'Play, leading to faster installs and adding
@@ -367,13 +372,22 @@ module.exports = {
           {
             test: sassRegex,
             exclude: sassModuleRegex,
-            loader: getStyleLoaders(
+            use: [
+              ...getStyleLoaders(
+                {
+                  importLoaders: 2,
+                  sourceMap: shouldUseSourceMap,
+                },
+                'sass-loader'
+              ),
               {
-                importLoaders: 2,
-                sourceMap: shouldUseSourceMap,
-              },
-              'sass-loader'
-            ),
+                loader: 'sass-resources-loader',
+                options: {
+                  // path to the file with resources
+                  resources: path.resolve(__dirname, '../src/constant/resources.scss'),
+                },
+              }
+            ],
             // Don't consider CSS imports dead code even if the
             // containing package claims to have no side effects.
             // Remove this when webpack adds a warning or an error for this.
